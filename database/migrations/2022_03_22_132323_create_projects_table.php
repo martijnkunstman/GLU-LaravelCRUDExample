12752b2c96904965d3a5f6104718594621f191c8
martijnkunstman <martijn@kunstman.nl>
2022-03-22 14:33:36 +0100

    projects add

---
+++ database/migrations/2022_03_22_132323_create_projects_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('desc');
+            $table->date('date');
+            $table->string('url'); 
+            $table->string('img'); 
+            $table->boolean('active')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+};
